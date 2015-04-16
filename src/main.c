a18189be522dd0a6eef5ec8def4495e99c0858cc
eugen0329 <eugen0329@gmail.com>
2015-04-16 01:55:04 +0000

    Implemented debug version support, declared output macros, minor cleanup, fixed cmakelist flags

--- src/main.c
+++ src/main.c
@@ -22,22 +22,28 @@
 #define FORK_SUCCESS 0
 #define FORK_ERR  -1
 #define RVAL_ERR  -1
-#define DAEMON_IS_TERMINATED 1
-
 
 #define FORCE_INLINE static inline __attribute__((always_inline))
 
+#ifdef DEBUG    /* define debug output functions */
+# define DPUTS( str ) puts( str );
+# define DPRINTF( ... ) printf( __VA_ARGS__ );
+#else           /* just eat up the output functions if release */
+# define DPRINTF( ... )
+# define DPUTS( str )
+#endif
 
+typedef enum { WSTILL_WAIT, WTIME_IS_OUT, WIS_UNLOCKED } waitRVal_t;
 typedef struct {
     unsigned long onIdleTimeout;
     timespec_t onIdleRefreshRate;
-    timespec_t onBlankedRefreshRate;
+
     unsigned long onLockedIdleTimeout;
     timespec_t onLockedRefreshRate;
+
+    timespec_t onBlankedRefreshRate;
     char display[BUF_SIZE];
 } daemonConfigs_t;
-
-typedef enum { WSTILL_WAIT, WTIME_IS_OUT, WIS_UNLOCKED } waitRVal_t;
 
 void initDaemon();
 pid_t startScreensaver();
@@ -56,10 +62,10 @@ void onBlanked();
 void onLocked();
 
 const static daemonConfigs_t DEFAULTS = {
-    .onIdleTimeout = MIN_TO_MSEC(1./60),
+    .onIdleTimeout = MIN_TO_MSEC(1.5/60),
     .onIdleRefreshRate = { .tv_sec = 0, .tv_nsec = TO_NANO_SEC(0.2) },
     .onBlankedRefreshRate = { .tv_sec = 0, .tv_nsec = TO_NANO_SEC(0.2) },
-    .onLockedIdleTimeout =  MIN_TO_MSEC(1./60),
+    .onLockedIdleTimeout =  MIN_TO_MSEC(2./60),
     .onLockedRefreshRate = { .tv_sec = 0, .tv_nsec = TO_NANO_SEC(0.2) },
     .display = "\":0.0\""
 };
@@ -67,25 +73,22 @@ const static daemonConfigs_t DEFAULTS = {
 static XScreenSaverInfo *info;
 static Display *display;
 static daemonConfigs_t * configs;
-bool_t isTerminated = false;
+bool_t isActive;
 static pid_t bgPid;
 static pid_t xscrPid;
 
-
 int main(int argc, char *argv[])
 {
-    /*uint8_t waitStatus;*/
-    currState = onIdle;
-
     initDaemon();
     xscrPid = startScreensaver();
-    puts("Daemon is ready");
+    DPUTS("Daemon is ready");
 
-    while(true) {
+    currState = onIdle;
+    isActive = true;
+    while(isActive) {
         (*currState)();
     }
 
-    puts("Release of resources");
     free(configs);
     kill(xscrPid, SIGKILL);
     XFree(info);
@@ -96,10 +99,10 @@ int main(int argc, char *argv[])
 
 void onIdle()
 {
-    puts("Start again");
+    DPUTS("Start again");
     waitIdle(configs->onIdleTimeout, &configs->onIdleRefreshRate, 0);
     bgPid = appendBackground();
-    printf("Specified idle time (%lu ms) is reached.\n", configs->onIdleTimeout);
+    DPRINTF("Specified idle time (%lu ms) is reached.\n", configs->onIdleTimeout);
     currState = &onBlanked;
 }
 
@@ -107,15 +110,14 @@ void onBlanked()
 {
     xscreensaverCommand("-activate");
     waitXScreensaverUnblanked(&configs->onBlankedRefreshRate);
-    puts("\n\nExiting XScreensaver");
+    DPUTS("\n\nExiting XScreensaver");
     currState = &onLocked;
 }
 
 void onLocked()
 {
-    pid_t lockerPid;
     uint8_t waitResult;
-    lockerPid = runScrLocker();
+    pid_t lockerPid = runScrLocker();
 
     waitResult = waitUnlocked(lockerPid, &configs->onLockedRefreshRate, configs->onLockedIdleTimeout);
     if(waitResult == WIS_UNLOCKED) {
@@ -129,14 +131,10 @@ void onLocked()
 
 uint8_t waitUnlocked(pid_t lockerPid, const timespec_t* refreshRate, unsigned long timeout)
 {
-    /*pid_t result;*/
     while(true) {
         nanosleep(refreshRate, NULL);
         if(idleTimeIsOut(timeout)) return WTIME_IS_OUT;
         if(waitpid(lockerPid, NULL, WNOHANG) != 0) return WIS_UNLOCKED;
-        /*result = waitpid(lockerPid, NULL, WNOHANG);*/
-        /*if (result == 0) {*/ /*printf("scrlocker is alive\n");*/
-        /*} else {*/ /*printf("scrlocker is dead\n");*/ /*return WIS_UNLOCKED;*/ /*}*/
     }
 
     return 0;
@@ -145,18 +143,15 @@ uint8_t waitUnlocked(pid_t lockerPid, const timespec_t* refreshRate, unsigned lo
 pid_t appendBackground()
 {
     GtkWidget *window;
-    GdkColor color;
+    GdkColor color = (GdkColor){ .red = 0x0, .green = 0x0, .blue = 0x0 };
     pid_t cpid = fork();
 
     if(cpid == FORK_SUCCESS) {
         gtk_init(NULL, NULL);
         window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
         gtk_widget_realize(window);
-        gtk_window_fullscreen((GtkWindow*) window);
-        color.red = 0x0;
-        color.green = 0x0;
-        color.blue = 0x0;
         gtk_widget_modify_bg(window, GTK_STATE_NORMAL, &color);
+        gtk_window_fullscreen((GtkWindow*) window);
         gtk_widget_show(window);
         gtk_main();
     } else if(cpid == FORK_ERR)
@@ -167,20 +162,16 @@ pid_t appendBackground()
 
 void termhdl(int notused)
 {
-    isTerminated = true;
+    isActive = false;
 }
 
 void initDaemon()
 {
-    /*struct sigaction act;*/
-    /*sigset_t mask;*/
     char setDispCmd[BUF_SIZE];
 
     system("killall xscreensaver &>/dev/null");
-
     configs = malloc(sizeof(daemonConfigs_t));
     memcpy(configs, &DEFAULTS, sizeof(daemonConfigs_t));
-
     snprintf(setDispCmd, sizeof(setDispCmd),
             "export DISPLAY=%s", configs->display);
     system(setDispCmd);
@@ -191,13 +182,6 @@ void initDaemon()
         XCloseDisplay(display);
         abortem("XScreenSaverAllocInfo");
     }
-
-    /*sigaddset(&mask, SIGINT);*/
-    /*act.sa_handler = &termhdl;*/
-    /*act.sa_flags = 0;*/
-    /*act.sa_mask = mask;*/
-
-    /*if(sigaction(SIGINT, &act, NULL) == RVAL_ERR ) abortem("sigaction");*/
 }
 
 pid_t startScreensaver()
@@ -216,11 +200,11 @@ pid_t startScreensaver()
 uint8_t waitIdle(unsigned long timeout, const timespec_t * refreshRate, int opts)
 {
     bool_t repeat = true;
+
     if(opts & WNOHANG) {
         repeat = false;
     }
     while(repeat) {
-        /*if(isTerminated) return EXIT_FAILURE;*/
         if(idleTimeIsOut(timeout) == true) return WTIME_IS_OUT;
         nanosleep(refreshRate, NULL);
     }
@@ -231,8 +215,7 @@ uint8_t waitIdle(unsigned long timeout, const timespec_t * refreshRate, int opts
 FORCE_INLINE bool_t idleTimeIsOut(unsigned long timeout)
 {
     XScreenSaverQueryInfo(display, DefaultRootWindow(display), info);
-    if(info->idle >= timeout) return true;
-    return false;
+    return info->idle >= timeout? true : false;
 }
 
 pid_t runScrLocker()
@@ -245,7 +228,6 @@ pid_t runScrLocker()
 
     return cpid;
 }
-
 
 void waitXScreensaverUnblanked(const timespec_t * refreshRate)
 {
@@ -271,10 +253,9 @@ void waitXScreensaverUnblanked(const timespec_t * refreshRate)
 
 void xscreensaverCommand(char * cmd)
 {
-    char *const cargv[3] =
-        {"/usr/bin/xscreensaver-command", cmd, NULL};
-    pid_t cpid = fork();
+    char *const cargv[3] = {"/usr/bin/xscreensaver-command", cmd, NULL};
 
+    pid_t cpid = fork();
     if(cpid == FORK_SUCCESS) {
         if(execvp(cargv[0], &cargv[0]) == RVAL_ERR) abortem("exec");
     } else if(cpid == FORK_ERR)
